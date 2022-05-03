6e7a02525d5fded9bfb586bc140ba34ce55b3fbf
Shasa <shasa.thuo@gmail.com>
2022-05-03 23:31:29 +0300

    Reacty router added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './App.css';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Mail from "./components/Mail";
+import EmailList from "./components/EmailList";
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// }from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <Header/>
-      <div className="app__body">
-        <Sidebar />
+    <Router>
+      <div className="app">
+        <Header/>
+        <div className="app__body">
+          <Sidebar />
+          <Routes>
+              <Route path="/" element={<EmailList />} />
+              <Route path="/mail" element={<Mail />}/>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
