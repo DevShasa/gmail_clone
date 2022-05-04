f373c498ee96f51538015180ca93680044b8cdd6
Shasa <shasa.thuo@gmail.com>
2022-05-04 19:02:11 +0300

    email selection panel

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -2,10 +2,18 @@ import React from 'react'
 import "./EmailList.css"
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import ArrowDropDown from '@mui/icons-material/ArrowDropDown';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import {} from 
+import { 
+    ChevronLeft,  
+    ChevronRight , 
+    KeyboardHide, 
+    Inbox,
+    SupervisorAccount,
+    LocalOffer
+} from '@mui/icons-material';
+import Section from "./Section";
 
 function EmailList() {
     return (
@@ -13,16 +21,23 @@ function EmailList() {
             <div className="emailList__settings">
                 <div className="emailList__setttingsLeft">
                     <Checkbox />
-                    <IconButton> <ExpandMoreOutlinedIcon/> </IconButton>
+                    <IconButton> <ArrowDropDown/> </IconButton>
                     <IconButton> <RefreshIcon/> </IconButton>
                     <IconButton> <MoreVertIcon/> </IconButton>
 
                 </div>
                 <div className="emailList__setttingsRight">
-                    <IconButton> <ExpandMoreOutlinedIcon/> </IconButton>
-                    <IconButton> <RefreshIcon/> </IconButton>
-                    <IconButton> <MoreVertIcon/> </IconButton>
+                    <IconButton> <ChevronLeft/> </IconButton>
+                    <IconButton> <ChevronRight/> </IconButton>
+                    <IconButton> <KeyboardHide/> </IconButton>
                 </div>
+            </div>
+
+            <div className="emailList__sections">
+                <Section Icon = {Inbox} title="Primary" color="red" selected />
+                <Section Icon = {SupervisorAccount} title="Social" color="#1A73E8"  />
+                <Section Icon = {LocalOffer} title="Promotions" color="green" />
+
             </div>
         </div>
     )
