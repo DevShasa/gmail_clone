6e6a4c853e2efe7ae67abfcc49225a46633f0f95
Shasa <shasa.thuo@gmail.com>
2022-05-04 16:44:23 +0300

    building main section

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import "./EmailList.css"
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {} from 
 
 function EmailList() {
     return (
         <div className="emailList">
-            EmailList
+            <div className="emailList__settings">
+                <div className="emailList__setttingsLeft">
+                    <Checkbox />
+                    <IconButton> <ExpandMoreOutlinedIcon/> </IconButton>
+                    <IconButton> <RefreshIcon/> </IconButton>
+                    <IconButton> <MoreVertIcon/> </IconButton>
+
+                </div>
+                <div className="emailList__setttingsRight">
+                    <IconButton> <ExpandMoreOutlinedIcon/> </IconButton>
+                    <IconButton> <RefreshIcon/> </IconButton>
+                    <IconButton> <MoreVertIcon/> </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
